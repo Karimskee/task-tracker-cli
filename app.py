03b9209ccc1595a5d96b88982a222eecf2fda506
Karimskee <kareemgamer2006@gmail.com>
2025-11-17 02:29:56 +0200

    Input logic

--- app.py
+++ app.py
@@ -11,8 +11,34 @@ import time
 
 def main():
     """Main program flow"""
-    # TODO: Inputting logic
     ...
+    # Print the opening user interface
+    opening_ui = "\n\
+        \rA great day to manage some tasks out eh?\n\
+        \r========================================\n\
+        "
+    print(opening_ui)
+
+    
+    # Handle user input
+    input = sys.argv
+    input_size = len(input)
+
+    if input_size <= 1: # Incorrect usage
+        print("Correct usage: python .\\task-cli.py [command] [argument 1] [argument 2] etc...\n")
+        print_commands()
+
+        
+    elif input[1] not in [command.name for command in commands]: # Incorrect command
+        print("Invalid command.")
+        print_commands()
+
+    
+    else:
+        command = [command for command in commands if input[1] == command.name][0]
+        command.execute_command(command, input)
+        
+    print()
 
 
 if __name__ == "__main__":
